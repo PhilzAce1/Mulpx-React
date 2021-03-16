7526f35d883f332b7ebddee45b854aadc05d446e
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-03-16 19:33:13 +0100

    need for scrollable div

--- src/components/test.js
+++ src/components/test.js
@@ -5,6 +5,12 @@ export default class test extends Component {
         return (
             <div>
                 <h3>Hello from test.</h3>
+
+                <div class="d-flex flex-row flex-nowrap">
+                    <div class="card card-block mx-2" style={{minWidth: "300px"}}>Card</div>
+                    <div class="card card-block mx-2" style={{minWidth: "300px"}}>Card</div>
+                    <div class="card card-block mx-2" style={{minWidth: "300px"}}>Card</div>            
+                </div>
             </div>
         )
     }
